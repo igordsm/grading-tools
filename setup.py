2a8600b3a5b2c355e2803a52f54d7fa66bf41495
Igor Montagner <igordsm@gmail.com>
2020-08-27 17:17:51 -0300

    Add dependency for pip.

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/igordsm/grading_tools",
+    install_requires=['psutil'],
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
